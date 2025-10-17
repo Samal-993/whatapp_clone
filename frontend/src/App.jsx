fbd298eb98f96ba2409225d02af52e864cb20885
Deepak <dipaksamal993@gmail.com>
2025-10-17 19:44:01 +1400

    SignUp_Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
-import { Route, Routes } from "react-router";
+import React, { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import SignUpPages from "./pages/SignUpPages";
 import LoginPages from "./pages/LoginPages";
 import { useAuthStore } from "./store/useAuthStore";
-
+import PageLoader from "./component/PageLoader";
+import {Toaster} from "react-hot-toast"
 const App = () => {
-  const {authUser, login, isLoggedIn} = useAuthStore();
-  console.log("Auth user",authUser);
-  console.log("isLoading",isLoggedIn);
   
+  const {checkAuth, isCheckingAuth, authUser}=useAuthStore()
+
+   useEffect(() => {
+    checkAuth()
+   },[checkAuth])
+
+   console.log(authUser)
+
+   if(isCheckingAuth)return<PageLoader/>
   
   
   return (
@@ -17,12 +24,13 @@ const App = () => {
        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"/>
         <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]"/>
         <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]"/>
-      <button onClick={login}>Button</button>
+     
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/signup" element={<SignUpPages />} />
-        <Route path="/login" element={<LoginPages />} />
+        <Route path="/" element={authUser ? <ChatPage/> : <Navigate to={"/login"}/>} />
+        <Route path="/login" element={!authUser?<LoginPages />: <Navigate to={"/"}/> } />
+        <Route path="/signup" element={!authUser ? <SignUpPages/>:  <Navigate to={"/"}/>} />
       </Routes>
+      <Toaster/>
     </div>
   );
 };
--- frontend/src/pages/LoginPages.jsx
+++ frontend/src/pages/LoginPages.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LoginPages = () => {
   return (
     <div>
-      loginpage
+      loginpage  
     </div>
   )
 }
